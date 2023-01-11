059fbaf8a8007b904f545d6af3e14472c5a12fb1
Sergey Borodavka <94472325+DrBATCOH@users.noreply.github.com>
2023-01-11 22:36:22 +0100

    added methods for working with data types: list

---
+++ lesson_3/list.py
@@ -0,0 +1,13 @@
+""" list """
+
+listt = [1,2,3,4,4]
+listt.append(5)
+listt.pop(1)
+print(listt.count(4))
+listt.index(3)
+listt[-1] = 7
+print(listt)
+print(listt[::-1])
+listt.reverse()
+del listt[1]
+print(listt)
