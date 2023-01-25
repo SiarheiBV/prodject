7a1125818d7639912867be728e207b95d75e735c
Sergey Borodavka <94472325+DrBATCOH@users.noreply.github.com>
2023-01-25 21:10:30 +0100

    added completed 4 task

---
+++ lesson_7/task_4.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from time import sleep, time
+
+
+def decorator(func):
+    def wrapper(*args):
+        start = time()
+        func(*args)
+        end = time()
+        print("\n", f"function execution time: {round(end - start, 10)} second")
+
+    return wrapper
+
+
+@decorator
+def fib_for(n):
+    a, b = 0, 1
+
+    for _ in range(1, n + 1):
+        print(a, end=' ')
+        a, b = b, a + b
+
+
+@decorator
+def romb(n):
+    for i in range(n):
+        print(' ' * (n - i) + ' *' * (i + 1))
+    for j in range(n - 1):
+        print(' ' * (j + 2) + ' *' * (n - 1 - j))
+
+
+fib_for(50)
+romb(5)
