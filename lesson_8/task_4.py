19c6fcd57116bc2ef64ffb9251a36ee579eb137e
DrBATCOH <borodavkasv111@gmail.com>
2023-02-02 22:50:25 +0800

    added completed task 4 and csv fail

---
+++ lesson_8/task_4.py
@@ -0,0 +1,9 @@
+import json
+import csv
+
+with open("taks3.json", "r") as file_json, open("task4.csv", "w") as file_csv:
+    file_j = json.load(file_json)
+    csv_wr = csv.DictWriter(file_csv, delimiter="-", fieldnames=["id", "name", "age", "phone"])
+    csv_wr.writeheader()
+    for k, v in file_j.items():
+        csv_wr.writerow({'id': k, 'name': v[0], 'age': v[1], "phone": input("enter phone number: ")})
